fa5a27dae1a94b95a8a06f81adc563cda675589a
vla5924 <vla2000@inbox.ru>
2020-09-18 14:53:56 +0300

    added config sample

---
+++ config/config.sample.php
@@ -0,0 +1,26 @@
+<?php
+
+// Copy this file into 'config.php' and insert your values.
+
+define('DATABASE_OPTIONS', [
+    'user' => '',
+    'pass' => '',
+    'db'   => ''
+]);
+
+define('TELEGRAM_TOKEN', '');
+
+define('VK_TOKEN', '');
+
+define('PROXY_SETTINGS', [
+    'address'  => '',
+    'port'     => '',
+    'username' => '',
+    'password' => ''
+]);
+
+define('SOURCE_LOGIN', '');
+
+define('SOURCE_PASSWORD', '');
+
+define('VK_PEER_ID', 0);
